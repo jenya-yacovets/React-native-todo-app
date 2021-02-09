b956913f7758d9aa7a00a2ad93bc32412698cf42
Jenya Yacovets <jenya.yacovets@gmail.com>
2021-02-09 18:03:26 +0300

    Inition react native app

---
+++ App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>React native todo app!</Text>
+      <StatusBar style="auto"/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 30,
+    color: 'yellow'
+  }
+});
